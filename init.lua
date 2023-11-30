76689bab3ca70a27a0913ded0e7e19c495b4dd17
arthur-k-kochi <asaarthurs@outlook.com>
2023-12-01 02:18:34 +0900

    update

--- init.lua
+++ init.lua
@@ -161,7 +161,7 @@ require('noice').setup({
 
 require('tokyonight').setup({
   style = 'night',
-  transparent = true,
+--  transparent = true,
   styles = {
     comments = { italic = true },
     keywords = { italic = true },
